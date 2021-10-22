0f4e12d5d0f1ef6ec2eace92aa9fd53fb0d1479b
ioanarijnita <ioanarijnita@gmail.com>
2021-10-22 03:02:06 +0300

    changed db config data for heroku server

--- PW-Backend/app/config/db.config.js
+++ PW-Backend/app/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "magazin",
+    HOST: "eu-cdbr-west-01.cleardb.com",
+    USER: "b6a6160d244967",
+    PASSWORD: "e3d069bf",
+    DB: "heroku_ca99e2492659f71",
     dialect: "mysql",
     pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     }
-  };
+};
